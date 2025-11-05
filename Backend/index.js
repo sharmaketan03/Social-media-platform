5c3bb549360d5c61d77bc17584fd72c97f2679e7
sharmaketan03 <ketan301024@gmail.com>
2025-11-05 12:22:24 +0530

    try push

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 ConnectDB();
 
 const corsOptions = {
-  origin:  "http://localhost:5173",
+  origin: [ "http://localhost:5173",process.env.FrontendLocalRender],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
